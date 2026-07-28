adbf90c2816e64b4af9f7b715271eb31d0ca87b0
Joel <jowel.dcm@gmail.com>
2026-07-28 11:48:51 -0500

    Pachinko & Introducción a C#

---
+++ MyUnity/Assets/_Main/Scripts/Player.cs
@@ -0,0 +1,17 @@
+
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update");
+    }
+}
