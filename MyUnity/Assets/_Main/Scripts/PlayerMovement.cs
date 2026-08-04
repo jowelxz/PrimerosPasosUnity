1cdad161b7591baf164b5ba8107fa4d3d6198197
jowelxz <jowel.dcm@gmail.com>
2026-08-04 11:26:19 -0500

    Variables

--- MyUnity/Assets/_Main/Scripts/PlayerMovement.cs
+++ MyUnity/Assets/_Main/Scripts/PlayerMovement.cs
@@ -1,31 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEditor.XR;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     //Variables
-    [SerializeField] private int edad = 18;
-    private float velocidad = 15.5f;
-    private string nombre = "Joel";
-    private bool IsJumping = false;
+ 
+    public float altura = 1.68f;
+    public int edad = 18;
+    public string nombre = "Joel";
+    public bool puedeVotar = true;
 
-    public float fuerza = 500f;
+    public GameObject gameObject;
+    public Rigidbody2D rigidbody2D;
+    public Collider2D collider2D;
+    public SpriteRenderer spriteRenderer;
 
-    [SerializeField] private Rigidbody2D _rigidbody2D;
+    //Crear una variable de tipo transform para almacenar la posición del jugador
+
+    public Transform transform;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-     
-        _rigidbody2D.AddForce(Vector2.right * fuerza);
+    private void Start()
+ {
+        Debug.Log("Hola: " + nombre + " Tu edad es: " + edad + " Tu altura es: " + altura);
+        rigidbody2D.simulated = false;
+        spriteRenderer.color = Color.red;
+
+        //Llamo la variable de tipo transform y le asigno un valor en  x de 10
+
+        transform.position = new Vector3(10, 0, 0);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ private void update()
+ {
+
+ }
+
 }
