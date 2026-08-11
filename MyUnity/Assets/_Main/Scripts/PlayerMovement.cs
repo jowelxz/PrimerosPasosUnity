a0d85fa8b90a4e0e596068cbdaa9ff71e0ad13d9
jowelxz <jowel.dcm@gmail.com>
2026-08-11 11:58:58 -0500

    GroundCheck

--- MyUnity/Assets/_Main/Scripts/PlayerMovement.cs
+++ MyUnity/Assets/_Main/Scripts/PlayerMovement.cs
@@ -9,23 +9,22 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _force = 5f;
     [SerializeField] private float _speed = 5f;
     [SerializeField] private Rigidbody2D _rigidbody2D;
-    
+    [SerializeField] private GroundCheck _groundCheck;
 
-    private void Start()
+    private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
-
     }
-
-    private void Update()
+    private void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (_groundCheck.isGround)
         {
-            _rigidbody2D.AddForce(Vector2.up * _force);
-            
+            if (Input.GetKey(KeyCode.Space))
+            {
+                _rigidbody2D.AddForce(Vector2.up * _force);
+                Debug.Log("Oprimi la tecla");
+            }
         }
         _rigidbody2D.velocity = Vector2.right * _speed;
-
     }
-
 }
